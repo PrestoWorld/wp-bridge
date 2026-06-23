bf92bf570c0a4a5f3841834ccd1cfd3261bc6f49
Puleeno Nguyen <puleeno@gmail.com>
2026-06-23 07:31:38 +0700

    refactor: group wp-admin routes under a common prefix for better maintainability

--- routes/wp-admin.php
+++ routes/wp-admin.php
@@ -14,13 +14,16 @@ $router->get('/wp-admin', function () {
     exit;
 });
 
-$router->get('/wp-admin/', \App\Http\Controllers\Admin\SpaController::class);
-$router->get('/wp-admin/index.php', \App\Http\Controllers\Admin\SpaController::class);
-$router->get('/wp-admin/admin.php', \App\Http\Controllers\Admin\SpaController::class);
-$router->get('/wp-admin/edit.php', \App\Http\Controllers\Admin\SpaController::class);
-$router->get('/wp-admin/plugins.php', \App\Http\Controllers\Admin\SpaController::class);
-$router->get('/wp-admin/options-general.php', \App\Http\Controllers\Admin\SpaController::class);
-$router->get('/wp-admin/themes.php', \App\Http\Controllers\Admin\SpaController::class);
-$router->post('/wp-admin/themes.php', [\App\Http\Controllers\Admin\AdminApiController::class, 'activateThemeFromForm']);
-$router->get('/wp-admin/admin-ajax.php', [\App\Http\Controllers\Admin\SpaController::class, 'adminAjax']);
-$router->post('/wp-admin/admin-ajax.php', [\App\Http\Controllers\Admin\SpaController::class, 'adminAjax']);
+$router->group(['prefix' => '/wp-admin'], function () use ($router) {
+    $router->get('', \App\Http\Controllers\Admin\SpaController::class);
+    $router->get('/', \App\Http\Controllers\Admin\SpaController::class);
+    $router->get('/index.php', \App\Http\Controllers\Admin\SpaController::class);
+    $router->get('/admin.php', \App\Http\Controllers\Admin\SpaController::class);
+    $router->get('/edit.php', \App\Http\Controllers\Admin\SpaController::class);
+    $router->get('/plugins.php', \App\Http\Controllers\Admin\SpaController::class);
+    $router->get('/options-general.php', \App\Http\Controllers\Admin\SpaController::class);
+    $router->get('/themes.php', \App\Http\Controllers\Admin\SpaController::class);
+    $router->post('/themes.php', [\App\Http\Controllers\Admin\AdminApiController::class, 'activateThemeFromForm']);
+    $router->get('/admin-ajax.php', [\App\Http\Controllers\Admin\SpaController::class, 'adminAjax']);
+    $router->post('/admin-ajax.php', [\App\Http\Controllers\Admin\SpaController::class, 'adminAjax']);
+});
